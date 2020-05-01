b1f4a72011adf173828a6903188d8152a2e38e99
Carlos Alexandre <carlos.nerware@gmail.com>
2020-05-01 18:30:04 -0300

    Primeiro controller e teste na página

--- SalesWebMvc/SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMvc.Controllers
 
 		public IActionResult About()
 		{
-			ViewData["Message"] = "Your application description page.";
+			ViewData["Message"] = "Salles Web MVC App";
+			ViewData["Owner"] = "Desenvolvido por Carlos Alexandre";
+			
 
 			return View();
 		}

7e83df2037686bb9dfcde31e250f406f8a332fe5
Carlos Alexandre <carlos.nerware@gmail.com>
2020-05-03 16:57:14 -0300

    Validação

--- SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
@@ -35,7 +35,12 @@ namespace SalesWebMvc.Controllers
 		[ValidateAntiForgeryToken]
 		public IActionResult Create(Seller seller)
 		{
-
+			if (!ModelState.IsValid)
+			{
+				var departments = _departmentService.FindAll();
+				var viewmodel = new SellerFormViewModel { Seller = seller, Departments = departments };
+				return View(viewmodel);
+			}
 			_sellerService.Insert(seller);
 			return RedirectToAction(nameof(Index));
 		}
@@ -95,7 +100,13 @@ namespace SalesWebMvc.Controllers
 		[ValidateAntiForgeryToken]
 		public IActionResult Edit(int id, Seller seller)
 		{
-			if(id != seller.Id)
+			if (!ModelState.IsValid)
+			{
+				var departments = _departmentService.FindAll();
+				var viewmodel = new SellerFormViewModel { Seller = seller, Departments = departments };
+				return View(viewmodel);
+			}
+			if (id != seller.Id)
 			{
 				return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
 			}
